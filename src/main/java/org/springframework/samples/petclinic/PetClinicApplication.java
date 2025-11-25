c71e646c6c5982d1f278fde4a99c19e367154cb9
Jiachen Tian <tjc990806@gmail.com>
2025-11-25 01:11:14 +0100

    read from file

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,36 +1,45 @@
-/*
- * Copyright 2012-2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
-/**
- * PetClinic Spring Boot Application.
- *
- * @author Dave Syer
- */
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+		loadDbSecretsFromFile();
+
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
+	private static void loadDbSecretsFromFile() {
+		setPropFromFile("DB_HOST", "DB_HOST_FILE");
+		setPropFromFile("DB_USER", "DB_USER_FILE");
+		setPropFromFile("DB_PASSWORD", "DB_PASSWORD_FILE");
+		setPropFromFile("DB_NAME", "DB_NAME_FILE");
+	}
+
+	private static void setPropFromFile(String propName, String fileEnv) {
+		String filePath = System.getenv(fileEnv);
+
+		if (filePath == null) {
+			System.err.println("Env not found: " + fileEnv);
+			return;
+		}
+
+		try {
+			String value = Files.readString(Path.of(filePath)).trim();
+			System.setProperty(propName, value);
+			System.out.println("Loaded " + propName + " from " + filePath);
+		}
+		catch (IOException e) {
+			System.err.println("Failed to read " + filePath + ": " + e.getMessage());
+		}
+	}
 }
